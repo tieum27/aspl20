91d39d12bf04d863de9748a6356bfb7d9876202a
root <root@DESKTOP-B2OAAM0.localdomain>
2018-01-11 15:59:45 -0800

    Added the gymnases to the view of Lieux and the corresponding transportations

--- app/controllers/lieux_controller.rb
+++ app/controllers/lieux_controller.rb
@@ -1,7 +1,8 @@
 class LieuxController < ApplicationController
   def index
-    @piscine = Equipement.where("type_equipement='Piscine'")
+    @piscines = Equipement.where("type_equipement='Piscine'")
     @transport = Transport.order(ligne: :ASC).where(:type_transport => 'Metro')
     @transportations = Transportation.all
+    @gymnases = Equipement.where("type_equipement='Gymnase'")
   end
 end
