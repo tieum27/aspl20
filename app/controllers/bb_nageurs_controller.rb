501fb8d80185bc3db2cf0f9c092d8b16d321913d
root <root@DESKTOP-B2OAAM0.localdomain>
2018-01-12 10:13:37 -0800

    Added bebe nageur page with db query

--- app/controllers/bb_nageurs_controller.rb
+++ app/controllers/bb_nageurs_controller.rb
@@ -1,4 +1,6 @@
 class BbNageursController < ApplicationController
   def index
+      @group1 = ActiviteAquatique.where(nom: 'BB Nageurs', age_group: '1')
+      @group2 = ActiviteAquatique.where(nom: 'BB Nageurs', age_group: '2')
   end
 end
--- app/controllers/natation_controller.rb
+++ app/controllers/natation_controller.rb
@@ -1,9 +1,8 @@
 class NatationController < ApplicationController
   def index
-      @group1 = ActiviteAquatique.where("age_group='1'")
-      @group2 = ActiviteAquatique.where("age_group='2'")
-      @group3 = ActiviteAquatique.where("age_group='3'")
+      @group1 = ActiviteAquatique.where(nom: 'Natation Enfants', age_group: '1')
+      @group2 = ActiviteAquatique.where(nom: 'Natation Enfants', age_group: '2')
+      @group3 = ActiviteAquatique.where(nom: 'Natation Enfants', age_group: '3')
       @natationAdultes = ActiviteAquatique.where("nom='Natation Adultes'")
-
   end
 end
