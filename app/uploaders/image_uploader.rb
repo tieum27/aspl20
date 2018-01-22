573a78d29e162ae4dabe94309b38ae393d417fe5
Matthieu Tripoli <tieum27@hotmail.com>
2018-01-22 12:23:14 -0800

    Heroku fix

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -21,8 +21,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
-  Process files as they are uploaded:
-  process scale: [200, 300]
+  # Process files as they are uploaded:
+  # process scale: [200, 300]
 
   def scale(width, height)
     # do something
